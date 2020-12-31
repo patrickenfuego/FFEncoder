c89b1c7c28d7c2a7a18e8cab8d9201813169244d
patrickenfuego <patrick.m.kelly1989@gmail.com>
2020-12-31 00:23:49 -0500

    tested function and working in main script

--- modules/FFTools/Public/Invoke-FFMpeg.ps1
+++ modules/FFTools/Public/Invoke-FFMpeg.ps1
@@ -2,24 +2,24 @@ function Invoke-FFMpeg  {
     [CmdletBinding()]
     param (
         # Parameter help description
-        [Parameter(Mandatory = $true)]
-        [Alias("InFile")]
+        [Parameter(Mandatory = $true, Position = 0)]
+        [Alias("InFile", "I")]
         [string]$InputFile,
 
         # Parameter help description
-        [Parameter(AttributeValues)]
+        [Parameter(Mandatory = $true, Position = 1)]
         [Alias("Crop", "CropDim")]
         [int[]]$CropDimensions,
 
         # Parameter help description
-        [Parameter(AttributeValues)]
+        [Parameter(Mandatory = $false)]
         [Alias("Audio", "A")]
-        [string]$AudioInput,
+        [string]$AudioInput = "none",
 
         # Parameter help description
-        [Parameter(AttributeValues)]
+        [Parameter(Mandatory = $false)]
         [Alias("P")]
-        [string]$Preset,
+        [string]$Preset = "slow",
 
         # Parameter help description
         [Parameter(Mandatory = $false)]
@@ -27,11 +27,26 @@ function Invoke-FFMpeg  {
 
         # Parameter help description
         [Parameter(Mandatory = $false)]
+        [Alias("DBF")]
         [int[]]$Deblock = @(-1, -1),
 
         # Parameter help description
+        [Parameter(Mandatory = $true, Position = 2)]
+        [hashtable]$HDR,
+
+        # Parameter help description
         [Parameter(Mandatory = $true)]
-        [hashtable]$HDR
+        [Alias("O")]
+        [string]$OutputPath,
+
+        # Parameter help description
+        [Parameter(Mandatory = $true)]
+        [Alias("L")]
+        [string]$LogPath,
+
+        # Parameter help description
+        [Parameter(Mandatory = $false)]
+        [switch]$Test
 
     )
 
@@ -39,15 +54,15 @@ function Invoke-FFMpeg  {
 
     Write-Host "Starting ffmpeg...`nTo view your progress, run 'gc path\to\crop.txt -Tail 10' in a different PowerShell session"
     if ($Test) {
-        ffmpeg -probesize 100MB -ss 00:01:00 -i $InputFile $audio -frames:v 1000 -vf "crop=w=$($cropDim[0]):h=$($cropDim[1])" `
-            -color_range tv -c:v libx265 -preset $Preset -crf $CRF -pix_fmt yuv420p10le `
+        ffmpeg -probesize 100MB -ss 00:01:00 -i $InputFile $audio -frames:v 1000 -vf "crop=w=$($CropDimensions[0]):h=$($CropDimensions[1])" `
+            -color_range tv -c:v libx265 -preset $Preset -crf $CRF -pix_fmt $HDR.PixelFmt `
             -x265-params "level-idc=5.1:keyint=120:deblock=$($Deblock[0]),$($Deblock[1]):sao=0:rc-lookahead=48:subme=4:colorprim=$($HDR.ColorPrimaries):`
             transfer=$($HDR.Transfer):colormatrix=$($HDR.ColorSpace):chromaloc=2:$($HDR.MasterDisplay)L($($HDR.MaxLuma),$($HDR.MinLuma)):max-cll=$($HDR.MaxCLL),$($HDR.MaxFAL):hdr-opt=1" `
             $OutputPath 2>$logPath
     }
     else {
-        ffmpeg -probesize 100MB -ss 00:01:00 -i $InputFile $audio -vf "crop=w=$($cropDim[0]):h=$($cropDim[1])" `
-            -color_range tv -c:v libx265 -preset $Preset -crf $CRF -pix_fmt yuv420p10le `
+        ffmpeg -probesize 100MB -ss 00:01:00 -i $InputFile $audio -vf "crop=w=$($CropDimensions[0]):h=$($CropDimensions[1])" `
+            -color_range tv -c:v libx265 -preset $Preset -crf $CRF -pix_fmt $HDR.PixelFmt `
             -x265-params "level-idc=5.1:keyint=120:deblock=$($Deblock[0]),$($Deblock[1]):sao=0:rc-lookahead=48:subme=4:colorprim=$($HDR.ColorPrimaries):`
             transfer=$($HDR.Transfer):colormatrix=$($HDR.ColorSpace):chromaloc=2:$($HDR.MasterDisplay)L($($HDR.MaxLuma),$($HDR.MinLuma)):max-cll=$($HDR.MaxCLL),$($HDR.MaxFAL):hdr-opt=1" `
             $OutputPath 2>$logPath
