52e4c9db4007a8ff0f4c2f94845ffe046c035ad1
patrickenfuego <patrick.m.kelly1989@gmail.com>
2021-01-02 18:16:12 -0500

    updated comments and console msgs

--- modules/FFTools/Private/Set-AudioPreference.ps1
+++ modules/FFTools/Private/Set-AudioPreference.ps1
@@ -1,7 +1,12 @@
 <#
-    Helper function which sets the audio encoding method
+    Helper function which builds the audio argument array for ffmpeg
+
+    .PARAMETER InputFile
+        The source file. This is used to determine the number of audio channels for lossy encoding
     .PARAMETER UserChoice
         Options are copy (passthrough), aac, and none
+    .PARAMETER AacBitrate
+        The constant bitrate to be used with ffmpeg's native AAC encoder. Value is the bitrate per audio channel
 #>
 function Set-AudioPreference {
     [CmdletBinding()]
@@ -14,18 +19,27 @@ function Set-AudioPreference {
         [ValidateNotNullOrEmpty()]
         [string]$UserChoice,
 
-        # Parameter help description
         [Parameter(Mandatory = $false, Position = 2)]
         [int]$AacBitrate
     )
 
-    if ($UserChoice -match "^c[opy]?") { return @('-c:a', 'copy') }
+    if ($UserChoice -match "^c[opy]?") { 
+        Write-Host "** COPY AUDIO SELECTED **" @progressColors
+        Write-Host "Audio stream 0 will be copied. " -NoNewline
+        Write-Host "If you are attempting to copy a Dolby Atmos stream, FFENCODER WILL FAIL as ffmpeg does not yet know how to decode it`n" @warnColors
+        return @('-c:a', 'copy') 
+    }
     elseif ($UserChoice -match "aac") {
         [int]$numOfChannels = ffprobe -i $InputFile -show_entries stream=channels -select_streams a:0 -of compact=p=0:nk=1 -v 0
         $bitrate = "$($numOfChannels * $AacBitrate)k"
-        Write-Host "Input file has $numOfChannels channels. Total AAC bitrate: $bitrate`bps" 
+        Write-Host "** AAC AUDIO SELECTED **" @progressColors
+        Write-Host "Primary audio stream has $numOfChannels channels. Total AAC bitrate: ~ $bitrate`n" 
         return @('-c:a', 'aac', '-b:a', $bitrate)
     }
-    elseif ($UserChoice -match "^n[one]?") { return '-an' }
-    else { Write-Verbose "No matching audio preference was found. Audio will not be copied"; return '-an' }
+    elseif ($UserChoice -match "^n[one]?") { 
+        Write-Host "** NO AUDIO SELECTED **" @progressColors
+        Write-Host "All audio streams will be excluded from the file`n"
+        return '-an' 
+    }
+    else { Write-Warning "No matching audio preference was found. Audio will not be copied`n"; return '-an' }
 }
--- modules/FFTools/Public/Get-HDRMetadata.ps1
+++ modules/FFTools/Public/Get-HDRMetadata.ps1
@@ -21,8 +21,11 @@ function Get-HDRMetadata {
     #Constants for mastering display color primaries
     Set-Variable -Name Display_P3 -Value "master-display=G(13250,34500)B(7500,3000)R(34000,16000)WP(15635,16450)" -Option Constant
     Set-Variable -Name BT_2020 -Value "master-display=G(8500,39850)B(6550,2300)R(35400,14600)WP(15635,16450)" -Option Constant
+
+    Write-Host "`nRetrieving HDR Metadata..." 
+
     #Gather HDR metadata using ffprobe
-    $probe = ffprobe -hide_banner -loglevel warning -select_streams v -print_format json `
+    $probe = ffprobe -hide_banner -loglevel error -select_streams v -print_format json `
         -show_frames -read_intervals "%+#1" -show_entries "frame=color_space,color_primaries,color_transfer,side_data_list,pix_fmt" `
         -i $InputFile
 
@@ -59,7 +62,13 @@ function Get-HDRMetadata {
         MaxCLL         = $maxCLL
         MaxFAL         = $maxFAL
     }
-    return $metadataObj
+    if ($null -eq $metadataObj) {
+        throw "HDR object is null. ffprobe may have failed to retrieve the data. Reload the module and try again, or run ffprobe manually to investigate."
+    }
+    else {
+        Write-Host "** HDR METADATA SUCCESSFULLY RETRIEVED **" @progressColors
+        return $metadataObj
+    }
 }
 
 
