977b522a1eecf98015b284de8dc1460778c09723
patrickenfuego <patrick.m.kelly1989@gmail.com>
2021-01-01 22:50:05 -0500

    changed switch to if/elseif bc it looks better

--- modules/FFTools/Private/Set-AudioPreference.ps1
+++ modules/FFTools/Private/Set-AudioPreference.ps1
@@ -15,17 +15,13 @@ function Set-AudioPreference {
         [Parameter(Mandatory = $false, Position = 2)]
         [int]$AacBitrate
     )
-    switch -Regex ($UserChoice) {
-        #If 'c' or 'copy' is selected
-        { $_ -match "^c[opy]?" } { return @('-c:a', 'copy') }
-        #If aac is selected, prompt for quality level (uses CBR for native ffmpeg aac)
-        { $_ -match "aac" } {
-            [int]$numOfChannels = ffprobe -i $InputFile -show_entries stream=channels -select_streams a:0 -of compact=p=0:nk=1 -v 0
-            $bitrate = $numOfChannels * $AacBitrate
-            return @('-c:a', 'aac', '-b:a', $bitrate)
-        }
-        #If 'n' or 'none' is selected. This is also the default behavior
-        { $_ -match "^n[one]?" } { return '-an' }
-        default { return '-an' }
+
+    if ($UserChoice -match "^c[opy]?") { return @('-c:a', 'copy') }
+    elseif ($UserChoice -match "aac") {
+        [int]$numOfChannels = ffprobe -i $InputFile -show_entries stream=channels -select_streams a:0 -of compact=p=0:nk=1 -v 0
+        $bitrate = $numOfChannels * $AacBitrate
+        return @('-c:a', 'aac', '-b:a', $bitrate)
     }
+    elseif ($UserChoice -match "^n[one]?") { return '-an' }
+    else { Write-Verbose "No matching audio preference was found. Audio will not be copied" }
 }
