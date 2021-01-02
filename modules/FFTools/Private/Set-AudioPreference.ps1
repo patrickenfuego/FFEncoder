7ec455bf6b72eb64b24f5e4b91512233c1349199
patrickenfuego <patrick.m.kelly1989@gmail.com>
2021-01-02 12:32:27 -0500

    Added console output and fixed default condition

--- modules/FFTools/Private/Set-AudioPreference.ps1
+++ modules/FFTools/Private/Set-AudioPreference.ps1
@@ -4,11 +4,14 @@
         Options are copy (passthrough), aac, and none
 #>
 function Set-AudioPreference {
+    [CmdletBinding()]
     param (
         [Parameter(Mandatory = $true, Position = 0)]
+        [ValidateNotNullOrEmpty()]
         [String]$InputFile,
 
         [Parameter(Mandatory = $true, Position = 1)]
+        [ValidateNotNullOrEmpty()]
         [string]$UserChoice,
 
         # Parameter help description
@@ -19,9 +22,10 @@ function Set-AudioPreference {
     if ($UserChoice -match "^c[opy]?") { return @('-c:a', 'copy') }
     elseif ($UserChoice -match "aac") {
         [int]$numOfChannels = ffprobe -i $InputFile -show_entries stream=channels -select_streams a:0 -of compact=p=0:nk=1 -v 0
-        $bitrate = $numOfChannels * $AacBitrate
+        $bitrate = "$($numOfChannels * $AacBitrate)k"
+        Write-Host "Input file has $numOfChannels channels. Total AAC bitrate: $bitrate`bps" 
         return @('-c:a', 'aac', '-b:a', $bitrate)
     }
     elseif ($UserChoice -match "^n[one]?") { return '-an' }
-    else { Write-Verbose "No matching audio preference was found. Audio will not be copied" }
+    else { Write-Verbose "No matching audio preference was found. Audio will not be copied"; return '-an' }
 }
