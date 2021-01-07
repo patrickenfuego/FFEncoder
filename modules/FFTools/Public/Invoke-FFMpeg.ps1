2b04088129e10bf5bd93fe10f1d43b2941f2685f
patrickenfuego <patrick.m.kelly1989@gmail.com>
2021-01-06 22:19:55 -0500

    removed open gop

--- modules/FFTools/Public/Invoke-FFMpeg.ps1
+++ modules/FFTools/Public/Invoke-FFMpeg.ps1
@@ -91,7 +91,7 @@ function Invoke-FFMpeg {
         Write-Host "Test Run Enabled. Encoding $TestFrames frames`n" @warnColors
         ffmpeg -probesize 100MB -ss 00:01:00 -i $InputFile -frames:v $TestFrames -vf "crop=w=$($CropDimensions[0]):h=$($CropDimensions[1])" `
             -color_range tv -c:v libx265 $audio -preset $Preset -crf $CRF -pix_fmt $HDR.PixelFmt `
-            -x265-params "level-idc=5.1:keyint=120:deblock=$($Deblock[0]),$($Deblock[1]):sao=0:rc-lookahead=48:subme=4:colorprim=$($HDR.ColorPrimaries):`
+            -x265-params "level-idc=5.1:open-gop=0:keyint=120:deblock=$($Deblock[0]),$($Deblock[1]):sao=0:rc-lookahead=48:subme=4:colorprim=$($HDR.ColorPrimaries):`
             transfer=$($HDR.Transfer):colormatrix=$($HDR.ColorSpace):chromaloc=2:$($HDR.MasterDisplay)L($($HDR.MaxLuma),$($HDR.MinLuma)):max-cll=$($HDR.MaxCLL),$($HDR.MaxFAL):hdr-opt=1" `
             $OutputPath 2>$logPath
     }
