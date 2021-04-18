942acdfa4e0b193f67d0a6a8180215a5313bec7a
patrickenfuego <patrick.m.kelly1989@gmail.com>
2021-04-18 12:32:34 -0400

    fixed bug when setting parameter defaults

--- modules/FFTools/Private/Set-Parameters.ps1
+++ modules/FFTools/Private/Set-Parameters.ps1
@@ -10,16 +10,16 @@ function Set-Parameters {
     )
     #Settings for x265 default variables based on preset
     switch ($Preset) {
-        'ultrafast'   { $pSubme = 0; $pBIntra = 0; $pBframes = 3; $pPsyRdoq = 0 }
-        'superfast'   { $pSubme = 1; $pBIntra = 0; $pBframes = 3; $pPsyRdoq = 0 }
-        'veryfast'    { $pSubme = 1; $pBIntra = 0; $pBframes = 4; $pPsyRdoq = 0 }
-        'faster'      { $pSubme = 2; $pBIntra = 0; $pBframes = 4; $pPsyRdoq = 0 }
-        'fast'        { $pSubme = 2; $pBIntra = 0; $pBframes = 4; $pPsyRdoq = 0 }
-        'medium'      { $pSubme = 2; $pBIntra = 0; $pBframes = 4; $pPsyRdoq = 0 }
-        'slow'        { $pSubme = 3; $pBIntra = 0; $pBframes = 4; $pPsyRdoq = 1.00 }
-        'slower'      { $pSubme = 4; $pBIntra = 1; $pBframes = 8; $pPsyRdoq = 1.00 }
-        'veryslow'    { $pSubme = 4; $pBIntra = 1; $pBframes = 8; $pPsyRdoq = 1.00 }
-        'placebo'     { $pSubme = 5; $pBIntra = 1; $pBframes = 8; $pPsyRdoq = 1.00 }
+        'ultrafast'   { $pSubme = 0; $pBIntra = 0; $pBframes = 3; $pPsyRdoq = 0; $pAqMode = 0 }
+        'superfast'   { $pSubme = 1; $pBIntra = 0; $pBframes = 3; $pPsyRdoq = 0; $pAqMode = 0 }
+        'veryfast'    { $pSubme = 1; $pBIntra = 0; $pBframes = 4; $pPsyRdoq = 0; $pAqMode = 2 }
+        'faster'      { $pSubme = 2; $pBIntra = 0; $pBframes = 4; $pPsyRdoq = 0; $pAqMode = 2 }
+        'fast'        { $pSubme = 2; $pBIntra = 0; $pBframes = 4; $pPsyRdoq = 0; $pAqMode = 2 }
+        'medium'      { $pSubme = 2; $pBIntra = 0; $pBframes = 4; $pPsyRdoq = 0; $pAqMode = 2 }
+        'slow'        { $pSubme = 3; $pBIntra = 0; $pBframes = 4; $pPsyRdoq = 1; $pAqMode = 2 }
+        'slower'      { $pSubme = 4; $pBIntra = 1; $pBframes = 8; $pPsyRdoq = 1; $pAqMode = 2 }
+        'veryslow'    { $pSubme = 4; $pBIntra = 1; $pBframes = 8; $pPsyRdoq = 1; $pAqMode = 2 }
+        'placebo'     { $pSubme = 5; $pBIntra = 1; $pBframes = 8; $pPsyRdoq = 1; $pAqMode = 2 }
         default       { throw "Unrecognized preset option in Set-Parameters" }
     }
     #If user passes custom params, set them instead of preset defaults
@@ -32,13 +32,17 @@ function Set-Parameters {
     else { $bframes = $pBframes }
 
     if ($ScriptParams.PsyRdoq) { $psyRdoq = $ScriptParams.PsyRdoq }
-    else { $psyRdoq = $psyRdoq }
+    else { $psyRdoq = $pPsyRdoq }
+
+    if ($ScriptParams.AqMode) { $aqMode = $ScriptParams.AqMode }
+    else { $aqMode = $pAqMode }
 
     $params = @{
         Subme   = $subme
         BIntra  = $bIntra
         BFrames = $bframes
         PsyRdoq = $psyRdoq
+        AqMode  = $aqMode
     }
 
     return $params
