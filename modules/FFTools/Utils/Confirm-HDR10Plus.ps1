fcd48fe166fb328b8fad340ac62dad36246abd21
patrickenfuego <patrick.m.kelly1989@gmail.com>
2021-09-19 15:58:56 -0400

    Add native HDR10+ parsing support

--- modules/FFTools/Utils/Confirm-HDR10Plus.ps1
+++ modules/FFTools/Utils/Confirm-HDR10Plus.ps1
@@ -18,34 +18,58 @@ function Confirm-HDR10Plus {
         [Parameter(Mandatory = $true, Position = 1)]
         [string]$HDR10PlusPath
     )
-    #Verify if HDR10+ parser is available via PATH. If not, return from function call
-    if (!(Get-Command 'hdr10plus_parser' -ErrorAction Ignore)) {
-        Write-Verbose "HDR10+ parser not found in PATH"
-        return
+
+
+    #Load parser path for UNIX systems
+    if ($IsLinux -or $IsMacOS) {
+        $parserPath = $IsLinux ?
+        (Join-Path ((Get-Item $PSScriptRoot).Parent.Parent.Parent) -ChildPath "bin/linux/hdr10plus_tool") :
+        (Join-Path ((Get-Item $PSScriptRoot).Parent.Parent.Parent) -ChildPath "bin/mac/hdr10plus_tool")
+        
+        #Verify that the parser path exists
+        if (!(Test-Path -Path $parserPath)) {
+            Write-Warning "Could not verify path to hdr10plus_parser. Metadata will be skipped"
+            return $false
+        }
+        #Change permissions and escape UNIX path
+        bash -c "chmod u+x '$parserPath'"
+        $parserPath = [regex]::Escape($parserPath)
     }
+    #Add parser to PATH on Windows systems
+    else {
+        $path = Join-Path ((Get-Item $PSScriptRoot).Parent.Parent.Parent) -ChildPath "bin\windows"
+        $env:PATH += ";$path"
+
+        #Verify that the parser is available via PATH
+        if (!(Get-Command 'hdr10plus_tool' -ErrorAction Ignore)) {
+            Write-Warning "hdr10plus_parser parser not found in PATH. Metadata will be skipped"
+            return $false
+        }
+    }
+
     #UNIX platforms. Escape meta characters for bash shell
     #Verifies if the source is HDR10 compatible
     if ($IsLinux -or $IsMacOS) {
         $InputFile = [regex]::Escape($InputFile)
-        $res = bash -c "ffmpeg -loglevel panic -i $InputFile -c:v copy -vbsf hevc_mp4toannexb -f hevc - | hdr10plus_parser --verify -"
+        $res = bash -c "ffmpeg -loglevel panic -i $InputFile -map 0:v:0 -c:v copy -vbsf hevc_mp4toannexb -f hevc - | $parserPath extract -"
     }
     #Windows platform. Requires cmd
-    ##Verifies if the source is HDR10 compatible
+    #Verifies if the source is HDR10 compatible
     else {
-        $res = cmd.exe /c "ffmpeg -loglevel panic -i `"$InputFile`" -c:v copy -vbsf hevc_mp4toannexb -f hevc - | hdr10plus_parser --verify -"
+        $res = cmd.exe /c "ffmpeg -loglevel panic -i `"$InputFile`" -map 0:v:0 -c:v copy -vbsf hevc_mp4toannexb -f hevc - | hdr10plus_tool extract -"
     }
     #If last command completed successfully and found metadata, generate json file
-    if ($? -and ($res[1] -like "*HDR10+*" -or $res -like "*HDR10+*")) {
+    if ($? -and $res -eq "Dynamic HDR10+ metadata detected.") {
         Write-Host "HDR10+ SEI metadata found..." -NoNewline
         if (Test-Path -Path $HDR10PlusPath) { Write-Host "JSON metadata file already exists" @warnColors }
         else {
             Write-Host "Generating JSON file" @emphasisColors
             if ($IsLinux -or $IsMacOS) {
                 $HDR10PlusPath = [regex]::Escape($HDR10PlusPath)
-                bash -c "ffmpeg -loglevel panic -i $InputFile -c:v copy -vbsf hevc_mp4toannexb -f hevc - | hdr10plus_parser -o $HDR10PlusPath -"
+                bash -c "ffmpeg -loglevel panic -i $InputFile -map 0:v:0 -c:v copy -vbsf hevc_mp4toannexb -f hevc - | $parserPath extract -o $HDR10PlusPath -"
             }
             else {
-                cmd.exe /c "ffmpeg -loglevel panic -i `"$InputFile`" -c:v copy -vbsf hevc_mp4toannexb -f hevc - | hdr10plus_parser -o `"$HDR10PlusPath`" -"
+                cmd.exe /c "ffmpeg -loglevel panic -i `"$InputFile`" -map 0:v:0 -c:v copy -vbsf hevc_mp4toannexb -f hevc - | hdr10plus_tool extract -o `"$HDR10PlusPath`" -"
             }
         }
         return $true
