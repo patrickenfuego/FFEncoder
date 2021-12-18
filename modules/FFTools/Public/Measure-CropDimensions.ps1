3f27e1724cb4630b7bf9ffd07eb74d1ca649a77e
patrickenfuego <patrick.m.kelly1989@gmail.com>
2021-12-18 00:15:42 -0500

    Fix rescale crop console output

--- modules/FFTools/Public/Measure-CropDimensions.ps1
+++ modules/FFTools/Public/Measure-CropDimensions.ps1
@@ -9,10 +9,16 @@ function Measure-CropDimensions
     [CmdletBinding()]
     param (
         [Parameter(Mandatory = $true, Position = 0)]
-        [string]$CropFilePath
+        [string]$CropFilePath,
+
+        [Parameter(Mandatory = $false, Position = 1)]
+        [AllowNull()]
+        [string]$Resolution
     )
 
-    if (!$CropFilePath) { throw "There was an issue reading the crop file. This usually happens when an empty file was generated on a previous run." }
+    if (!$CropFilePath) { 
+        throw "There was an issue reading the crop file. This usually happens when an empty file was generated on a previous run." 
+    }
     Write-Host "`nScanning crop file for dimensions..."
     $cropFile = Get-Content $CropFilePath
     $cropHeight = 0
@@ -29,11 +35,36 @@ function Measure-CropDimensions
     
     if ($cropWidth -eq 0 -or $cropHeight -eq 0) {
         throw "One or both of the crop values are equal to 0. Check the input path and try again."
+        exit 2
     }
-    elseif ($cropWidth -gt 3000) { $enableHDR = $true }
+    elseif ($cropWidth -ge 3000) { $enableHDR = $true }
     else { $enableHDR = $false }
-    
+
     Write-Host "** CROP DIMENSIONS SUCCESSFULLY RETRIEVED ** " @progressColors
-    Write-Host "Dimensions: $cropWidth x $cropHeight`n"
+    if (!$PSBoundParameters['Resolution']) {
+        Write-Host "Dimensions: $cropWidth x $cropHeight`n"
+    }
+    elseif ($Resolution -eq '1080p') {
+        switch ($cropWidth) {
+            { $_ -ge 3000 }   { Write-Host "Dimensions (Downscaled): $($cropWidth / 2) x $($cropHeight / 2)`n" }
+            { $_ -le 1280 }   { Write-Host "Dimensions (Upscaled): $($cropWidth * 1.5) x $([math]::Ceiling($cropHeight * 1.5))`n" }
+        }
+    }
+    elseif ($Resolution -eq '2160p') {
+        switch ($cropWidth) {
+            { $_ -gt 1280 -and $_ -le 1920 }  { Write-Host "Dimensions (Upscaled): $($cropWidth * 2) x $($cropHeight * 2)`n" }
+            { $_ -le 1280 }                   { Write-Host "Dimensions (Upscaled): $($cropWidth * 3) x $($cropHeight * 3)`n" }
+        }
+    }
+    elseif ($Resolution -eq '720p') {
+        switch ($cropWidth) {
+            { $_ -ge 3000 }                   { Write-Host "Dimensions (Downscaled): $($cropWidth / 3) x $([math]::Ceiling($cropHeight / 3))`n" }
+            { $_ -gt 1280 -and $_ -le 1920}   { Write-Host "Dimensions (Downscaled): $($cropWidth / 1.5) x $([math]::Ceiling($cropHeight / 1.5))`n" }
+        }
+    }
+    else {
+        Write-Warning "Failed to print crop dimensions"
+    }
+
     return @($cropWidth, $cropHeight, $enableHDR) 
 }
