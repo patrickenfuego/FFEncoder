e939b0748f14a7e59c94d819ef578c5aa86b04e0
patrickenfuego <patrick.m.kelly1989@gmail.com>
2022-06-13 22:47:36 -0400

    Remove extra delete prompt
    It was broken, it only stopped execution in the script scope and did not record user response.

--- modules/FFTools/Utils/Remove-FilePrompt.ps1
+++ modules/FFTools/Utils/Remove-FilePrompt.ps1
@@ -1,3 +1,8 @@
+<#
+    .SYNOPSIS
+        Creates a PowerShell friendly prompt for deleting existing files
+#>
+
 function Remove-FilePrompt {
     [CmdletBinding()]
     param (
@@ -13,34 +18,49 @@ function Remove-FilePrompt {
     $title = "$Type Output Path Already Exists"
     $prompt = "Would you like to delete it?"
     $yesPrompt = New-Object System.Management.Automation.Host.ChoiceDescription "&Yes", 
-    "Delete the existing file. you will be asked to confirm again before proceeding"
+        "Delete the existing file. you will be asked to confirm again before proceeding"
     $noPrompt = New-Object System.Management.Automation.Host.ChoiceDescription "&No", 
-    "Do not delete the existing file and exit the script. The file must be renamed or deleted before continuing"
+        "Do not delete the existing file and exit the script. The file must be renamed or deleted before continuing"
     $options = [System.Management.Automation.Host.ChoiceDescription[]]($yesPrompt, $noPrompt)
     $response = $host.ui.PromptForChoice($title, $prompt, $options, 1)
 
     switch ($response) {
         0 { 
-            Remove-Item -Path $Path -Include "*.mkv", "*.mp4", "*.ts", "*.m2ts", "*.avi" -Confirm 
-            if ($?) { 
-                Write-Host "`nFile <" -NoNewline @progressColors 
-                Write-Host "$Path" -NoNewline @emphasisColors
-                Write-Host "> was successfully deleted`n" @progressColors 
+            [System.IO.File]::Delete($Path)
+            if ($?) {
+                if ($psReq) {
+                    $msg = "$aGreen`File $($aCyan+$ul)$Path$($ulOff)$aGreen was successfully deleted"
+                    Write-Host $msg
+                }
+                else {
+                    Write-Host "`nFile <" -NoNewline @progressColors 
+                    Write-Host "$Path" -NoNewline @emphasisColors
+                    Write-Host "> was successfully deleted`n" @progressColors
+                }   
             }
-            else { 
-                Write-Host "<" -NoNewline @warnColors
-                Write-Host $Path @emphasisColors
-                Write-Host "> could not be deleted. Make sure it is not in use by another process. Exiting script..." @warnColors
-                exit 2
+            else {
+                if ($psReq) {
+                    $msg = "$aRed`File $($aBlue+$ul)$Path$($ulOff+$aRed) could not be deleted. Make sure it is not in use by another process. Exiting script..."
+                    Write-Host $msg
+                }
+                else {
+                    Write-Host "<" -NoNewline @warnColors
+                    Write-Host $Path @emphasisColors
+                    Write-Host "> could not be deleted. Make sure it is not in use by another process. Exiting script..." @warnColors
+                    exit -69
+                }
+                
             }
         }
         1 { 
-            Write-Host  "Please choose a different file name, or delete the existing file. Exiting script..." @warnColors
+            Write-Host "Please choose a different file name, or delete the existing file. Exiting script..." @warnColors
             exit 0
         }
         default { 
             throw "An error occurred while attempting to delete <$Path> via prompt. This should be unreachable"
-            exit 2
+            exit -55
         }
     }
+
+    Write-Host ""
 }
