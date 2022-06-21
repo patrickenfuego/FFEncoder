e8da6ccea28f08868805c42283fb65fb6d212893
patrickenfuego <patrick.m.kelly1989@gmail.com>
2022-06-20 20:00:30 -0400

    Export dee from module
    Now public function in Utils directory. Add downmix support and logging

--- modules/FFTools/Utils/Invoke-DeeEncoder.ps1
+++ modules/FFTools/Utils/Invoke-DeeEncoder.ps1
@@ -30,46 +30,61 @@ function Invoke-DeeEncoder {
         [int]$ChannelCount,
 
         [Parameter(Mandatory = $false)]
-        [int]$Bitrate
+        [int]$Bitrate,
+
+        [Parameter(Mandatory = $false)]
+        [switch]$Stereo
     )
 
-    if (!(Get-Command 'dee')) {
-        Write-Warning "Could not find dee executable in PATH. Returning"
-        return
-    }
-
+    $logPath = [Path]::Join($(Split-Path $Paths.InputFile -Parent), 'dee.log')
     $outputPath = Split-Path $Paths.OutputFile -Parent
 
-    $codecStr = switch ($Codec) {
-        { $_ -in 'dee_dd', 'dee_ac3' }     { 'dd'  }
-        { $_ -in 'dee_ddp', 'dee_eac3' }   { 'ddp' }
-        'dee_thd'                          { 'thd' }
+    Write-Output "Preparing DEE encoder..." >$logPath
+    Write-Verbose "Parameters:`n`n$([pscustomobject]$PSBoundParameters | Out-String)" 4>>$logPath
+
+    if (!(Get-Command 'dee')) {
+        Write-Warning "Could not find dee executable in PATH. Returning" 3>&1 >>$logPath
+        return
+    }
+    # Set verbose for Invoke-mkvMerge because of scope change
+    if ($PSBoundParameters['Verbose']) { $setVerbose = $true }
+
+    $codecStr, $downmix = switch ($Codec) {
+        { $_ -in 'dee_dd', 'dee_ac3' -and !$Stereo }    { 'dd', $null  }
+        { $_ -in 'dee_dd', 'dee_ac3' -and $Stereo }     { 'dd', 2  }
+        { $_ -in 'dee_ddp', 'dee_eac3' -and $Stereo }   { 'ddp', 2 }
+        { $_ -in 'dee_ddp', 'dee_eac3' -and !$Stereo }  { 'ddp', $null }
+        { $_ -in 'dee_ddp_51', 'dee_eac3_51' }          { 'ddp', 6 }
+        'dee_thd'                                       { 'thd' }
     }
 
     $audioBase = ($Paths.InputFile.EndsWith('mkv')) ? ("$($Paths.Title)_audio.mka") : ("$($Paths.Title)_audio.m4a")
     $Paths.AudioPath = [Path]::Join($(Split-Path $Paths.InputFile -Parent), $audioBase)
 
-    if (!(Test-Path $Paths.AudioPath -ErrorAction SilentlyContinue)) {
-        Write-Host "Multiplexing audio..." @progressColors
+    if (![File]::Exists($Paths.AudioPath)) {
+        Write-Verbose "ThreadJob: Multiplexing audio for DEE...`n" 4>>$logPath
 
         $remuxPaths = @{
             Input    = $Paths.InputFile
             Output   = $Paths.AudioPath
             Title    = $Paths.Title
             Language = $Paths.Language
+            LogPath  = $Paths.LogPath
         }
         if ((Get-Command 'mkvmerge')) {
-            Invoke-MkvMerge -Paths $remuxPaths -Mode 'extract'
+            Write-Output "mkvmerge detected. Multiplexing audio stream..." >>$logPath
+            Invoke-MkvMerge -Paths $remuxPaths -Mode 'extract' -Verbose:$setVerbose
         }
         else {
+            Write-Output "Multiplexing audio stream with ffmpeg..." >>$logPath
             ffmpeg -i $Paths.InputFile -map 0:a:0 -c:a:0 copy -map -0:t? -map_chapters -1 $Paths.AudioPath
         }
 
         if ([File]::Exists($Paths.AudioPath) -and ([FileInfo]($Paths.AudioPath)).Length -lt 10) {
-            Write-Host "There was an issue extracting the audio track for dee" @warnColors
+            Write-Output "There was an issue extracting the audio track for dee" >>$logPath
             return
         }
-        Start-Sleep -Milliseconds 750
+        Start-Sleep -Milliseconds 500
     }
 
     # Create the directory structure
@@ -85,13 +100,22 @@ function Invoke-DeeEncoder {
         'Linux'   { [Path]::Join($binRoot, 'linux/dee_wrapper/deew') }
         'Mac'     { [Path]::Join($binRoot, 'mac/dee_wrapper/deew')  }
     }
+    
     if (!$deePath) {
+        Write-Output "No compatible OS found. Exiting..." >>$logPath
         return
     }
+    else {
+        Write-Output "deew path: $deePath`n" >>$logPath
+    }
     
+    # Modify the toml file with tmp directory
     $deeToml = [Path]::Join($($deePath -replace 'deew.*', ''), 'config.toml')
     (Get-Content $deeToml -Raw) -replace 'temp_path.*', "temp_path = '$tmpPath'" | 
         Set-Content $deeToml
+
+    Write-Verbose "Deew TOML contents:`n" 4>>$logPath
+    Write-Verbose "$((Get-Content $deeToml).ForEach({"$_`n"}))`n" 4>>$logPath
 
     # Setup encoder array
     $deeArgs = @(
@@ -105,15 +129,33 @@ function Invoke-DeeEncoder {
             '-b'
             $Bitrate
         }
+        if ($downmix) {
+            '-dm'
+            $downmix
+        }
     )
-    # Run the encoder
-    & $deePath $deeArgs
 
-    # Delete the temp file
-    if ($?) { 
-        Start-Sleep -Seconds 2
-        [Directory]::Delete($tmpPath)
-        #Remove-Item $tmpPath -Force
+    Write-Verbose "Dee Arguments:`n$($deeArgs | Out-String)" 4>>$logPath
+    
+    # Run the encoder
+    try {
+        if ($Stereo) {
+            # Pipe 'y' to avoid confirmation prompt for stereo
+            'y' | & $deePath $deeArgs 2>&1 >>$logPath
+        }
+        else {
+            & $deePath $deeArgs 2>&1 >>$logPath
+        }
+    }
+    catch {
+        Write-Error "An exception occurred running deew: $($_.Exception.Message). Returning...`n" 2>>$logPath
+        return
+    }
+   
+    # Delete the temp directory
+    if ($LASTEXITCODE) { 
+        Start-Sleep -Seconds 3
+        Remove-Item $tmpPath -Force -Recurse
     }
 }
 
